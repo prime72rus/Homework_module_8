e904355b85ae0c19d1924b29e59e488e5d660e04
Dmitrii Grechko <prime.optimys@mail.ru>
2025-06-15 21:47:53 +0500

    add users/models.py model User add users custom command createadmin.py

--- users/models.py
+++ users/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, verbose_name="Email")
+    phone = models.CharField(
+        max_length=35,
+        verbose_name="Телефон",
+        blank=True,
+        null=True,
+    )
+    city = models.CharField(
+        max_length=65,
+        verbose_name="Город",
+        blank=True,
+        null=True,
+    )
+    avatar = models.ImageField(
+        upload_to="avatars/",
+        verbose_name="Аватар",
+        blank=True,
+        null=True,
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
