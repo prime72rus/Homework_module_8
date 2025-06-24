9c635f1e3ce8364edae09aeba06251af5757fd9d
Dmitrii Grechko <prime.optimys@mail.ru>
2025-06-24 15:42:49 +0500

    add filters and ordering PaymentViewSet

--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.filters import OrderingFilter
 
 from users.models import User, Payment
 from users.serializers import UserSerializer, PaymentSerializer
@@ -12,3 +14,6 @@ class UserViewSet(viewsets.ModelViewSet):
 class PaymentViewSet(viewsets.ModelViewSet):
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ("paid_course", "paid_lesson", "payment_method")
+    ordering_fields = ("payment_date",)
