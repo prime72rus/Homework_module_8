2f1e5743a594e169fa5b73f2a916d63c6f356eff
Dmitrii Grechko <prime.optimys@mail.ru>
2025-06-24 13:14:30 +0500

    add model Payment in app users

--- users/models.py
+++ users/models.py
@@ -33,3 +33,25 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Payment(models.Model):
+
+    PAYMENT_METHOD_CHOICES = [
+        ("cach", "Наличные"),
+        ("transfer", "Перевод на счет"),
+    ]
+
+    user = models.ForeignKey("User", on_delete=models.SET_NULL, blank=True, null=True, related_name="users", verbose_name="Пользователь")
+    payment_date = models.DateTimeField()
+    paid_course = models.ForeignKey("materials.Course", on_delete=models.SET_NULL, blank=True, null=True, related_name="users", verbose_name="Оплаченный курс")
+    paid_lesson = models.ForeignKey("materials.Lesson", on_delete=models.SET_NULL, blank=True, null=True, related_name="users", verbose_name="Оплаченный урок")
+    amount = models.IntegerField(verbose_name="Сумма оплаты")
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHOD_CHOICES, default="cach", verbose_name="Способ оплаты")
+
+    def __str__(self):
+        return f"{self.user} - {self.payment_date} - {self.amount}"
+
+    class Meta:
+        verbose_name = "Платеж"
+        verbose_name_plural = "Платежи"
