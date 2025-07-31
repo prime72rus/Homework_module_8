da6060b59c965195c97fea9f1d9d29a683b47303
Dmitrii Grechko <prime.optimys@mail.ru>
2025-07-31 19:03:38 +0500

    edit schedule CELERY_BEAT_SCHEDULE settings.py

--- config/settings.py
+++ config/settings.py
@@ -188,7 +188,7 @@ CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 CELERY_BEAT_SCHEDULE = {
     'deactivate-inactive-users': {
         'task': 'users.tasks.deactivate_inactive_users',
-        'schedule': timedelta(minutes=1),
+        'schedule': timedelta(days=1),
     },
 }
 
