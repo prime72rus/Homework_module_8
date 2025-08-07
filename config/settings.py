d084bc7fb3ae28e5d501b9922cb132fae0ea5ca8
Dmitrii Grechko <prime.optimys@mail.ru>
2025-08-07 12:54:53 +0500

    add workflow GitHub Actions

--- config/settings.py
+++ config/settings.py
@@ -108,14 +108,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = "Asia/Yekaterinburg"
+AUTH_USER_MODEL = "users.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "Europe/Moscow"
+TIME_ZONE = "Asia/Yekaterinburg"
 
 USE_I18N = True
 USE_L18N = True
